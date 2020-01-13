1cf01a1175f86ad5f802ce3848f5766782dc8897
DurzooBlint <marcin.karpik@gmail.com>
2020-01-12 19:37:30 -0800

    Cleaning tools.py and adding gridSearch for all models

--- tools.py
+++ tools.py
@@ -51,64 +51,67 @@ def display_scores(scores, label):
 def get_best_model(samples, labels, hypersearch=False):
     # train different kind of models
     lin_reg = LinearRegression()
-    lin_reg.fit(samples, labels)
+    # lin_reg.fit(samples, labels)
     tree_reg = DecisionTreeRegressor()
-    tree_reg.fit(samples, labels)
-    random_forest_reg = RandomForestRegressor(n_estimators=300, max_features=6, min_samples_leaf=1, min_samples_split=2, n_jobs=-1)
-    random_forest_reg.fit(samples, labels)
+    # tree_reg.fit(samples, labels)
+    random_forest_reg = RandomForestRegressor()
+    # random_forest_reg.fit(samples, labels)
     svm_linear_reg = SVR()
-    svm_linear_reg2 = SVR(kernel='rbf', C=0.5)
-    svm_linear_reg3 = SVR(kernel='linear', gamma='auto')
-    svm_linear_reg.fit(samples, labels)
-    svm_linear_reg2.fit(samples, labels)
-    svm_linear_reg3.fit(samples, labels)
+    # svm_linear_reg2 = SVR(kernel='rbf', C=0.5)
+    # svm_linear_reg3 = SVR(kernel='linear', gamma='auto')
+    # svm_linear_reg.fit(samples, labels)
+    # svm_linear_reg2.fit(samples, labels)
+    # svm_linear_reg3.fit(samples, labels)
     # cross validate
-    lin_reg_scores = cross_val_score(lin_reg, samples, labels,
-                                     scoring="neg_mean_squared_error", cv=10)
-    tree_reg_scores = cross_val_score(tree_reg, samples, labels,
-                                      scoring="neg_mean_squared_error", cv=10)
-    random_forest_scores = cross_val_score(random_forest_reg, samples, labels,
-                                           scoring="neg_mean_squared_error", cv=10)
-    svm_linear_scores = cross_val_score(svm_linear_reg, samples, labels,
-                                        scoring="neg_mean_squared_error", cv=10)
-    svm_linear_scores2 = cross_val_score(svm_linear_reg2, samples, labels,
-                                        scoring="neg_mean_squared_error", cv=10)
-    svm_linear_scores3 = cross_val_score(svm_linear_reg3, samples, labels,
-                                        scoring="neg_mean_squared_error", cv=10)
-    lin_rmse_scores = (np.sqrt(-lin_reg_scores)).mean()
-    tree_rmse_scores = (np.sqrt(-tree_reg_scores)).mean()
-    forest_rmse_scores = (np.sqrt(-random_forest_scores)).mean()
-    svm_rmse_scores = (np.sqrt(-svm_linear_scores)).mean()
-    svm_rmse_scores2 = (np.sqrt(-svm_linear_scores2)).mean()
-    svm_rmse_scores3 = (np.sqrt(-svm_linear_scores3)).mean()
-
-    print('Linear Regression RMSE:', lin_rmse_scores)
-    print('Decision Tree Regression RMSE:', tree_rmse_scores)
-    print('Random Forst Regression RMSE:', forest_rmse_scores)
-    print('SVM linear Regression RMSE:', svm_rmse_scores)
-    print('SVM linear Regression2 RMSE:', svm_rmse_scores2)
-    print('SVM linear Regression3 RMSE:', svm_rmse_scores3)
-
-
-    # if (lin_rmse_scores < tree_rmse_scores) and (lin_rmse_scores < forest_rmse_scores):
-    #     param_grid = [{'fit_intercept': 'False', 'normalize': ['False', 'True'], 'n_jobs': [-1]},
-    #                   {'fit_intercept': 'True', 'normalize': ['False', 'True'], 'n_jobs': [-1]}]
-    #     model_best = lin_reg
-    # elif (tree_rmse_scores < lin_rmse_scores) and (tree_rmse_scores < forest_rmse_scores):
-    #     param_grid = [{'splitter': 'best', 'min_samples_split': [2, 4], 'min_samples_leaf': [1, 2, 4],
-    #                    'max_features': ['auto', 2, 4, 6, 8], 'n_jobs': -1},
-    #                   {'splitter': 'random', 'min_samples_split': [2, 4], 'min_samples_leaf': [1, 2, 4],
-    #                    'max_features': ['auto', 2, 4, 6, 8], 'n_jobs': -1}]
-    #     model_best = tree_reg
-    # else:
-    #     param_grid = {'n_estimators': [100, 200, 300], 'min_samples_split': [2, 4],
-    #                   'max_features': ['auto', 2, 4, 6, 8],
-    #                   'min_samples_leaf': [1, 2, 4], 'n_jobs': [-1]}
-    #     model_best = random_forest_reg
+    # lin_reg_scores = cross_val_score(lin_reg, samples, labels,
+    #                                  scoring="neg_mean_squared_error", cv=10)
+    # tree_reg_scores = cross_val_score(tree_reg, samples, labels,
+    #                                   scoring="neg_mean_squared_error", cv=10)
+    # random_forest_scores = cross_val_score(random_forest_reg, samples, labels,
+    #                                        scoring="neg_mean_squared_error", cv=10)
+    # svm_linear_scores = cross_val_score(svm_linear_reg, samples, labels,
+    #                                     scoring="neg_mean_squared_error", cv=10)
+    # svm_linear_scores2 = cross_val_score(svm_linear_reg2, samples, labels,
+    #                                     scoring="neg_mean_squared_error", cv=10)
+    # svm_linear_scores3 = cross_val_score(svm_linear_reg3, samples, labels,
+    #                                     scoring="neg_mean_squared_error", cv=10)
+    # lin_rmse_scores = (np.sqrt(-lin_reg_scores)).mean()
+    # tree_rmse_scores = (np.sqrt(-tree_reg_scores)).mean()
+    # forest_rmse_scores = (np.sqrt(-random_forest_scores)).mean()
+    # svm_rmse_scores = (np.sqrt(-svm_linear_scores)).mean()
+    # svm_rmse_scores2 = (np.sqrt(-svm_linear_scores2)).mean()
+    # svm_rmse_scores3 = (np.sqrt(-svm_linear_scores3)).mean()
     #
-    # grid_search = GridSearchCV(model_best, param_grid, cv=10,
-    #                            scoring='neg_mean_squared_error',
-    #                            return_train_score=True)
-    # grid_search.fit(samples, labels)
-    # print('GridSearch best params:', grid_search.best_params_)
-    # print('GridSearch RMSE:', grid_search.best_score_)
+    # print('Linear Regression RMSE:', lin_rmse_scores)
+    # print('Decision Tree Regression RMSE:', tree_rmse_scores)
+    # print('Random Forst Regression RMSE:', forest_rmse_scores)
+    # print('SVM linear Regression RMSE:', svm_rmse_scores)
+    # print('SVM linear Regression2 RMSE:', svm_rmse_scores2)
+    # print('SVM linear Regression3 RMSE:', svm_rmse_scores3)
+
+    # perform GridSearch for all models and print out score
+
+    param_grid_linear = param_grid = {'fit_intercept': ['False', 'True'], 'normalize': ['False', 'True'],
+                                      'n_jobs': [-1]}
+    param_grid_decision_tree = {'splitter': ['best', 'random'], 'min_samples_split': [2, 4], 'min_samples_leaf': [1, 2, 4],
+                               'max_features': ['auto', 2, 4, 6, 8]}
+    param_grid_random_forest = {'n_estimators': [100, 200, 300], 'min_samples_split': [2, 4],
+                               'max_features': ['auto', 2, 4, 6, 8], 'min_samples_leaf': [1, 2, 4], 'n_jobs': [-1]}
+    param_grid_svm = {'C': [0.001, 0.1, 10, 100, 10e5], 'gamma': ['scale', 'auto'], 'kernel': ['linear', 'rbf', 'poly']}
+
+    grid_search = GridSearchCV(lin_reg, param_grid_linear, cv=10)
+    grid_search.fit(samples, labels)
+    print('Linear Regression score = %3.2f' %(grid_search.score(samples,labels)))
+
+    grid_search = GridSearchCV(svm_linear_reg, param_grid_svm, cv=10)
+    grid_search.fit(samples, labels)
+    print('SVM score = %3.2f' %(grid_search.score(samples,labels)))
+
+    grid_search = GridSearchCV(tree_reg, param_grid_decision_tree, cv=10)
+    grid_search.fit(samples, labels)
+    print('Decision Tree Regression score = %3.2f' %(grid_search.score(samples,labels)))
+
+    grid_search = GridSearchCV(random_forest_reg, param_grid_random_forest, cv=10)
+    grid_search.fit(samples, labels)
+    print('Random Forest Regression score = %3.2f' %(grid_search.score(samples,labels)))
+
